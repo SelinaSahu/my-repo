ecb9c010157d3663cab888bab9d5c12ed1f314e5
SelinaSahu <147151046+SelinaSahu@users.noreply.github.com>
2024-06-16 12:35:39 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test
     public static void main(String[] args) {
          System.out.println("HELLO");
     System.out.println("Bye");
+        System.out.println("welcome!");
     }
 }
